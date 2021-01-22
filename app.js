ac4221cabb4df74d1c2d7d33f38b5e2f50e11d07
Hayden Sykes <haydensykes13@gmail.com>
2021-01-22 17:15:08 -0500

    initial commit for loadbalancer.  app takes requests and reroutes based on URL subdomain

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const request = require('request');
+
+//load env variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+const publicIP = process.env.PUBLIC_IP
+//all services are on same server, different ports
+//front end forwarding only
+const serverPorts = {
+    homebridge:process.env.HOMEBRIDGE_PORT,
+    alarm:process.env.ALARM_PORT
+}
+
+const handler = (req, res) => {
+    // Add an error handler for the proxied request
+    if(!req.get('host')){return null}
+    //console.log(req.get('host'))
+    const subDomain = req.get('host').split('.')[0] //split FQDN by '.' to find each part of URL
+    //console.log(subDomain)
+    const redirectUrl = 'http://'+publicIP + ':' + serverPorts[subDomain] //concat localIP:subdomainPort to get redirect
+    console.log(redirectUrl)
+
+    res.redirect(redirectUrl);
+    //req.pipe(_req).pipe(res);
+    
+};
+
+const server = express().get('*', handler).post('*', handler);
+
+server.listen(8099);
