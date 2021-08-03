4765837cfb4087a798fd0983b4141c03f1b43ee3
Hayden Sykes <haydensykes13@gmail.com>
2021-08-03 19:53:46 -0400

    add queryParams passing for alarm service

--- app.js
+++ app.js
@@ -9,22 +9,31 @@ const publicIP = process.env.PUBLIC_IP
 //all services are on same server, different ports
 //front end forwarding only
 const serverPorts = {
-    homebridge:process.env.HOMEBRIDGE_PORT,
-    alarm:process.env.ALARM_PORT
+    homebridge: process.env.HOMEBRIDGE_PORT,
+    alarm: process.env.ALARM_PORT
 }
 
 const handler = (req, res) => {
     // Add an error handler for the proxied request
-    if(!req.get('host')){return null}
+    if (!req.get('host')) { return null }
     //console.log(req.get('host'))
     const subDomain = req.get('host').split('.')[0] //split FQDN by '.' to find each part of URL
+
+    let queryParams;
+    switch (subDomain) {
+        case 'alarm':
+            queryParams = `?key=${req.query.key}&start=${req.query.start}&end=${req.query.end}`
+            break;
+        default:
+            queryParams = ''
+    }
     //console.log(subDomain)
-    const redirectUrl = 'http://'+publicIP + ':' + serverPorts[subDomain] //concat localIP:subdomainPort to get redirect
+    const redirectUrl = 'http://' + publicIP + ':' + serverPorts[subDomain] + queryParams //concat localIP:subdomainPort to get redirect
     console.log(redirectUrl)
 
     res.redirect(redirectUrl);
     //req.pipe(_req).pipe(res);
-    
+
 };
 
 const server = express().get('*', handler).post('*', handler);
